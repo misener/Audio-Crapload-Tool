9a6c7139971399d7a1300e031f6a53b62ffd2270
Dan <misener@misenermini.media.in.cbcsrc.ca>
2014-12-19 15:44:53 -0500

    Revert "Crap -> Carp"
    This reverts commit 13da0eb29cf754f36f142b5a36965180d2d9f9d9.

--- audiocraploadtool.user.js
+++ audiocraploadtool.user.js
@@ -2,12 +2,12 @@
 // @name       Audio Crapload Tool
 // @author       Dan Misener
 // @namespace  http://misener.org
-// @version    0.3
+// @version    0.2
 // @description  Have some fun with the Audio Upload Tool.
 // @include     https://cms.nm.cbc.ca/audiouploadtool/*
 // @match       https://cms.nm.cbc.ca/audiouploadtool/*
 // @updateURL	https://github.com/misener/Audio-Crapload-Tool/raw/master/audiocraploadtool.user.js
 // ==/UserScript==
  
-document.getElementsByTagName('h1')[0].innerHTML = "Audio Carpload Tool";
-document.getElementsByTagName('img')[0].src = "https://iowafarmbureau.files.wordpress.com/2011/06/carp11.jpg";
+document.getElementsByTagName('h1')[0].innerHTML = "Audio Crapload Tool";
+document.getElementsByTagName('img')[0].src = "http://emojipedia.org/wp-content/uploads/2013/08/160x160x91-pile-of-poo.png.pagespeed.ic.N6iNfrs6Op.jpg";
