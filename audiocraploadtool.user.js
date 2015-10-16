b83a01f6b69066e47569c819701a2a9be7ced07e
misener <dan@misener.org>
2015-10-16 16:40:37 -0400

    Update audiocraploadtool.user.js

--- audiocraploadtool.user.js
+++ audiocraploadtool.user.js
@@ -2,7 +2,7 @@
 // @name       Audio Crapload Tool
 // @author       Dan Misener
 // @namespace  http://misener.org
-// @version    0.3
+// @version    0.4
 // @description  Have some fun with the Audio Upload Tool.
 // @include     https://cms.nm.cbc.ca/audiouploadtool/*
 // @match       https://cms.nm.cbc.ca/audiouploadtool/*
@@ -10,4 +10,4 @@
 // ==/UserScript==
  
 document.getElementsByTagName('h1')[0].innerHTML = "Audio Crapload Tool";
-document.getElementsByTagName('img')[0].src = "http://emojipedia-us.s3.amazonaws.com/cache/de/ca/decadd7edb6b1014ca0cb7a1afcb8ea3.png";
+document.getElementsByTagName('img')[0].src = "https://raw.githubusercontent.com/misener/Audio-Crapload-Tool/master/pileofpoo.png";
